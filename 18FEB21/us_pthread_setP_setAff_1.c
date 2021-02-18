0cd6235e82663583f03a693868514d19eddd672d
vishnu10183 <vishnu.m@acufore.com>
2021-02-18 16:28:23 +0530

    Updated on 18FEB21 16:28

--- 18FEB21/us_pthread_setP_setAff_1.c
+++ 18FEB21/us_pthread_setP_setAff_1.c
@@ -62,7 +62,7 @@ void *ultraS( void *param ){
 		unsigned long int duration;
 		float cm;
 
-		if ( count <= 20 ) {
+		if ( count < 20 ) {
 		  pinMode(pingPin, OUTPUT);
 		  digitalWrite(pingPin, LOW);
 		  delayMicroseconds(2);
