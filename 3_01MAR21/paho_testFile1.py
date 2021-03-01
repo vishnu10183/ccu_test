0c03d4ee2623fdb4e9f5153342d919b73fd06ab4
vishnu10183 <vishnu.m@acufore.com>
2021-03-01 11:57:41 +0530

    Updated 01MAR (MQTT)

---
+++ 3_01MAR21/paho_testFile1.py
@@ -0,0 +1,31 @@
+import paho.mqtt.client as mqtt
+import time
+
+def onConnect( client, userdata, flags, rc ):
+    if rc == 0:
+        print( "Connection Successful!" )
+    elif rc == 1:
+        print( "[ERROR] Connection Error -", rc, " : Incorrect Protocol Version" )
+    elif rc == 2:
+        print( "[ERROR] Connection Error -", rc, " : Invalid Client Identifier" )
+    elif rc == 3:
+        print( "[ERROR] Connection Error -", rc, " : Server Unavailable" )
+    elif rc == 4:
+        print( "[ERROR] Connection Error -", rc, " : Bad Username/Password" )
+    elif rc == 5:
+        print( "[ERROR] Connection Error -", rc, " : Unauthorised Access!" )
+    else:
+        print( "[ERROR] Connection Error -", rc, " : Currently Unused" )
+
+
+
+if __name__ == '__main__':
+    ourClient = mqtt.Client( "Pi4-MQTT" ) # Create MQTT Client Object
+    
+    ourClient.on_connect = onConnect # Connection call-back function
+    ourClient.connect( "192.168.4.114"  ) # Broker IP address
+
+    # Publish a Message to "test-Pi" Topic
+    ourClient.publish( "test-Pi" , " Paho..." )
+
+    time.sleep(1)
